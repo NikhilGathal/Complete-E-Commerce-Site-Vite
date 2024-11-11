389f61c115b29012d7470d683971867d2275d8bf
Nikhil <nikhilgathal@gmail.com>
2024-11-11 15:25:15 +0530

    Feedback Form Message

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -4,7 +4,7 @@ function ContactForm() {
   
     
   return (
-    <div className='orderplace'>Feedback Submitted Successfully</div>
+    <div className="empty-wish-container"> <h1 className='empty-wish'> Feedback Submitted Successfully</h1> </div>
   )
 }
 
