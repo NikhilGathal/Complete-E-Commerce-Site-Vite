04c50f049a2553fcbbc4820611ffa729492e0612
Nikhil <nikhilgathal@gmail.com>
2024-12-11 10:07:29 +0530

    Changed Alert message

--- pages/Cart.jsx
+++ pages/Cart.jsx
@@ -194,7 +194,7 @@ export default function Cart() {
 
                     // Condition 1: Check if Admin exists in localStorage
                     if (!adminFromStorage) {
-                      alert("Sign up as an admin first before placing an order. After that, log in as a normal user to place an order.")
+                      alert("Sign up and login as an admin first before placing an order. After that, log in as a normal user to place an order.")
                       return
                     }
 
