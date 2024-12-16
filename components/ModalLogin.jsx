45fbe9898236084da8d13ed6d58ae6685c001bd3
Nikhil <nikhilgathal@gmail.com>
2024-12-16 17:02:43 +0530

    Added Forgot Password functionality

--- components/ModalLogin.jsx
+++ components/ModalLogin.jsx
@@ -1,143 +1,232 @@
 
-import React, { useState } from 'react';
-import { createPortal } from 'react-dom';
-import './ModalLogin.css';
-import { removeallCartItem } from '../store/slices/cartSlice';
-import { useDispatch } from 'react-redux';
-import { removeallWishItem } from '../store/slices/wishListSlice';
-import { useNavigate } from 'react-router-dom';
 
-export default function ModalLogin({ islog, setislog  ,setusername ,setuserlogin , setIsAdmin}) {
+import React, { useState } from 'react'
+import { createPortal } from 'react-dom'
+import './ModalLogin.css'
+import { removeallCartItem } from '../store/slices/cartSlice'
+import { useDispatch } from 'react-redux'
+import { removeallWishItem } from '../store/slices/wishListSlice'
+import { useNavigate } from 'react-router-dom'
 
-    const existingAdmin = JSON.parse(localStorage.getItem('Admin')) || {}
-    // console.log(existingAdmin);
-    
-    const [loginData, setLoginData] = useState({
-        username: '',
-        password: ''
-    });
+export default function ModalLogin({
+  islog,
+  setislog,
+  setusername,
+  setuserlogin,
+  setIsAdmin,
+}) {
+  const existingAdmin = JSON.parse(localStorage.getItem('Admin')) || {}
 
-    const navigate = useNavigate();
-    const dispatch = useDispatch()
+  const [loginData, setLoginData] = useState({
+    username: '',
+    password: '',
+  })
 
-    // Handle input changes
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setLoginData((prevData) => ({
-            ...prevData,
-            [name]: value
-        }));
-    };
+  const [newPassword, setNewPassword] = useState({
+    password: '',
+    confirmPassword: '',
+  })
 
-    // Handle Log In (Validate with localStorage)
-    // const handleLogin = () => {
-    //     const existingUsers = JSON.parse(localStorage.getItem('users')) || [];
-    
-    //     // Find the user with the matching username and password
-    //     const user = existingUsers.find(
-    //         user => 
-    //             user.username === loginData.username && 
-    //             user.password === loginData.password
-    //     );
-    
-    //     if (user) {
-    //         alert('Successfully logged in!');
-    //         setusername(user.username);
-    //         localStorage.setItem('username', user.username); // Store the username in localStorage
-    //         dispatch(removeallCartItem());
-    //         dispatch(removeallWishItem());
-    //         setuserlogin(true);
-    //         setislog(false);
-    //     } else {
-    //         alert('Login failed. Username or password is incorrect.');
-    //     }
-    
-    //     setLoginData({
-    //         username: '',
-    //         password: ''
-    //     });
-    // };
+  const [isForgotPassword, setIsForgotPassword] = useState(false) // Track forgot password state
 
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
-    const handleLogin = () => {
-        const existingUsers = JSON.parse(localStorage.getItem('users')) || [];
+  // Handle input changes
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    if (isForgotPassword) {
+      setNewPassword((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }))
+    } else {
+      setLoginData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }))
+    }
+  }
 
-        // Admin credentials
+  const handleLogin = () => {
+    const existingUsers = JSON.parse(localStorage.getItem('users')) || []
 
-        // Check if login is for admin
-        if (loginData.username === existingAdmin.username && loginData.password === existingAdmin.password) {
-            alert('Admin logged in successfully!');
-            setusername(loginData.username);
-            localStorage.setItem('username', loginData.username);
-            setIsAdmin(true);  // Indicate user is an admin
-            setuserlogin(true);
-            setislog(false);
+    // Admin credentials
+    if (
+      loginData.username === existingAdmin.username &&
+      loginData.password === existingAdmin.password
+    ) {
+      alert('Admin logged in successfully!')
+      setusername(loginData.username)
+      localStorage.setItem('username', loginData.username)
+      setIsAdmin(true) // Indicate user is an admin
+      setuserlogin(true)
+      setislog(false)
+      navigate('/')
+      setLoginData({ username: '', password: '' })
+      return
+    }
 
-            navigate('/');
-            setLoginData({ username: '', password: '' });
-            return;
-        }
+    // Check if login is for a regular user
+    const user = existingUsers.find(
+      (user) =>
+        user.username === loginData.username &&
+        user.password === loginData.password
+    )
 
-        // Check if login is for a regular user
-        const user = existingUsers.find(
-            user => 
-                user.username === loginData.username && 
-                user.password === loginData.password
-        );
+    if (user) {
+      alert('Successfully logged in as User!')
+      setusername(user.username)
+      localStorage.setItem('username', user.username)
+      dispatch(removeallCartItem())
+      dispatch(removeallWishItem())
+      setuserlogin(true)
+      setIsAdmin(false) // Indicate user is a regular user
+      setislog(false)
+      navigate('/')
+    } else {
+      alert('Login failed. Username or password is incorrect.')
+    }
 
-        if (user) {
-            alert('Successfully logged in as User!');
-            setusername(user.username);
-            localStorage.setItem('username', user.username);
-            dispatch(removeallCartItem());
-            dispatch(removeallWishItem());
-            setuserlogin(true);
-            setIsAdmin(false); // Indicate user is a regular user
-            setislog(false);
-            navigate('/');
-        } else {
-            alert('Login failed. Username or password is incorrect.');
-        }
+    // Reset login data after attempt
+    setLoginData({ username: '', password: '' })
+  }
 
-        // Reset login data after attempt
-        setLoginData({
-            username: '',
-            password: ''
-        });
-    };
-    
+  const handleForgotPassword = () => {
+    // Check if the username field is empty
+    if (!loginData.username.trim()) {
+      alert('Please enter a valid username to reset the password.')
+      return // Prevent further execution if the username is empty
+    }
 
+    // Check if the username matches an existing user or admin
+    const existingUsers = JSON.parse(localStorage.getItem('users')) || []
+    if (loginData.username === existingAdmin.username) {
+      setIsForgotPassword(true) // Proceed to reset password for admin
+      return
+    }
 
+    const user = existingUsers.find(
+      (user) => user.username === loginData.username
+    )
+    if (!user) {
+      alert('Username does not exist!')
+      setLoginData({ username: '', password: '' }) // Show alert if username is not found
+      return
+    }
 
+    setIsForgotPassword(true) // Proceed to reset password for regular user
+  }
 
-    return createPortal(
-        <div onClick={() => setislog(false)} className={`modal-overlay-log ${islog ? '' : 'hidden'}`}>
-            <div onClick={(e) => e.stopPropagation()} className="modal-box-log">
-                <div className="modal-heading">LogIn</div>
-                <div className="modal-form">
-                    <input
-                        placeholder="Username"
-                        className="modal-input"
-                        type="text"
-                        name="username"
-                        value={loginData.username}
-                        onChange={handleChange}
-                    />
-                    <input
-                        placeholder="Password"
-                        className="modal-input"
-                        type="password"
-                        name="password"
-                        value={loginData.password}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className="modal-buttons">
-                    <button onClick={() => setislog(false)} className="cancel-button">Cancel</button>
-                    <button onClick={handleLogin} className="login-button">LogIn</button>
-                </div>
-            </div>
-        </div>,
-        document.getElementById('portal')
-    );
+  const handleSaveNewPassword = () => {
+    // Check if any password field is empty
+    if (!newPassword.password.trim() || !newPassword.confirmPassword.trim()) {
+      alert('Please enter valid password in both fields.')
+      return // Prevent further execution if any password field is empty
+    }
+
+    // Check if both passwords match
+    if (newPassword.password !== newPassword.confirmPassword) {
+      alert('Both passwords should match!')
+      setNewPassword({ password: '', confirmPassword: '' })
+      return // Prevent further execution if passwords do not match
+    }
+
+    const existingUsers = JSON.parse(localStorage.getItem('users')) || []
+    const userIndex = existingUsers.findIndex(
+      (user) => user.username === loginData.username
+    )
+
+    if (userIndex >= 0) {
+      existingUsers[userIndex].password = newPassword.password
+      localStorage.setItem('users', JSON.stringify(existingUsers))
+      alert('Password reset successfully!')
+      setIsForgotPassword(false) // Hide reset fields after successful reset
+      setLoginData({ username: '', password: '' })
+      setNewPassword({ password: '', confirmPassword: '' })
+    } else {
+      const updatedAdmin = { ...existingAdmin, password: newPassword.password }
+      localStorage.setItem('Admin', JSON.stringify(updatedAdmin))
+      alert('Admin password reset successfully!')
+      setLoginData({ username: '', password: '' })
+      setIsForgotPassword(false) // Hide reset fields after successful reset
+      setNewPassword({ password: '', confirmPassword: '' })
+    }
+  }
+  const m = () => {
+    setislog(false)
+    setIsForgotPassword(false)
+  }
+
+  return createPortal(
+    <div onClick={m} className={`modal-overlay-log ${islog ? '' : 'hidden'}`}>
+      <div onClick={(e) => e.stopPropagation()} className="modal-box-log">
+        <div className="modal-heading">
+          {isForgotPassword ? 'Reset Password' : 'LogIn'}
+        </div>
+        <div className="modal-form">
+          {isForgotPassword ? (
+            <>
+              <input
+                placeholder="Enter New Password"
+                className="modal-input"
+                type="password"
+                name="password"
+                value={newPassword.password}
+                onChange={handleChange}
+              />
+              <input
+                placeholder="Re-Enter New Password"
+                className="modal-input"
+                type="password"
+                name="confirmPassword"
+                value={newPassword.confirmPassword}
+                onChange={handleChange}
+              />
+            </>
+          ) : (
+            <>
+              <input
+                placeholder="Username"
+                className="modal-input"
+                type="text"
+                name="username"
+                value={loginData.username}
+                onChange={handleChange}
+              />
+              <input
+                placeholder="Password"
+                className="modal-input"
+                type="password"
+                name="password"
+                value={loginData.password}
+                onChange={handleChange}
+              />
+            </>
+          )}
+        </div>
+        <div className="modal-buttons">
+          {isForgotPassword ? (
+            <button onClick={handleSaveNewPassword} className="login-button">
+              Save
+            </button>
+          ) : (
+            <>
+              <button onClick={handleLogin} className="login-button">
+                LogIn
+              </button>
+              <button onClick={() => setislog(false)} className="cancel-button">
+                Cancel
+              </button>
+
+              <button onClick={handleForgotPassword} className="login-button">
+                Forgot Password
+              </button>
+            </>
+          )}
+        </div>
+      </div>
+    </div>,
+    document.getElementById('portal')
+  )
 }
